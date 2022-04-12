cf22b4acd5d7446c0f1a60e964d6d7bfd2c54aac
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-04-12 21:14:32 +0100

    .

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require("express")
-const app = express()
-require("dotenv").config()
-const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
-const bodyParser = require("body-parser")
-const cors = require("cors")
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
 
 app.post("/payment", cors(), async (req, res) => {
-  let { amount, id } = req.body
+  let { amount, id } = req.body;
   try {
     const payment = await stripe.paymentIntents.create({
       amount,
@@ -19,21 +23,21 @@ app.post("/payment", cors(), async (req, res) => {
       description: "New Car",
       payment_method: id,
       confirm: true,
-    })
-    console.log("Payment", payment)
+    });
+    console.log("Payment", payment);
     res.json({
       message: "Payment successful",
       success: true,
-    })
+    });
   } catch (error) {
-    console.log("Error", error)
+    console.log("Error", error);
     res.json({
       message: "Payment failed",
       success: false,
-    })
+    });
   }
-})
+});
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log("Sever is listening on port 4000")
-})
+  console.log("Sever is listening on port 4000");
+});
